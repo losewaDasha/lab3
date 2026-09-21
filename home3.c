9eb8848018bafcae184f0d72d974d125d9933765
losewaDasha <losewadasha@mail.ru>
2026-09-21 04:10:05 +0300

    Add files via upload

---
+++ home3.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_DEPRECATE
+#include <stdio.h>
+#include <locale.h>
+
+#define cc 0.3
+int main()
+{
+	setlocale(LC_CTYPE, "RUS");
+	int f;
+	float lam1;
+	float lam2;
+	puts("Введите число");
+	scanf("%d", &f);
+	lam1 = cc / f;
+	printf("\n---Результаты:---\n");
+	printf("При частоте %d ГГц длина волны (в метрах) равна %.4f м\n", lam1);
+	printf("При частоте %d ГГц длина волны (в миллиметрах) равна %.2f мм\n", lam1*1000);
+
+}
